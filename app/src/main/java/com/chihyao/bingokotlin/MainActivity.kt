548d1019500bc87aaf842ea363640dcbe4669f65
RogerLi <>
2021-07-20 23:32:58 +0800

    init commit

---
+++ app/src/main/java/com/chihyao/bingokotlin/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.chihyao.bingokotlin
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.AuthUI.IdpConfig.EmailBuilder
+import com.firebase.ui.auth.AuthUI.IdpConfig.GoogleBuilder
+import com.google.firebase.auth.FirebaseAuth
+import java.util.*
+
+
+class MainActivity : AppCompatActivity(), FirebaseAuth.AuthStateListener {
+    companion object{
+        val TAG = MainActivity::class.java.simpleName
+        val RC_SIGN_IN = 100
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        FirebaseAuth.getInstance().addAuthStateListener(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        FirebaseAuth.getInstance().removeAuthStateListener(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_menu_signout -> {
+                FirebaseAuth.getInstance().signOut()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onAuthStateChanged(auth: FirebaseAuth) {
+        auth.currentUser?.also {
+            Log.d(TAG, "onAuthStateChanged: ${it.email}/${it.uid}")
+        } ?: signUp()
+/*        if (auth.currentUser == null) {
+            signUp()
+        } else {
+            Log.d(TAG, "${auth.currentUser!!.email}/${auth.currentUser!!.uid}" )
+        }*/
+    }
+
+    private fun signUp() {
+
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(
+                    Arrays.asList(
+                        EmailBuilder().build(),
+                        GoogleBuilder().build()
+                    )
+                )
+                .setIsSmartLockEnabled(false)
+                .build(), RC_SIGN_IN
+        )
+    }
+}
